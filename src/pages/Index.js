35db2ffb000f4a5703a8bf5f7ed8e649523fce70
Stetson Harding <stetson2001@msn.com>
2021-08-07 09:01:56 -0700

    information displayed correctly form firebase.

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,33 +4,38 @@ import StateSelection from "../stateSelection/StateSelection";
 
 const Index = () => {
   const [ufoSightings, setUfoSightings] = useState([]);
-  const [userStateSelection, setUserStateSelection] = useState(
-    "No Location Selected"
-  );
-  // console.log(ufoSightings);
+  const [userStateSelection, setUserStateSelection] = useState("empty");
 
   useEffect(() => {
+    let allUfo = [];
+
     //referencing firebase db
     const ufoRef = firebase.database().ref("ufos");
     //filter database searching for spacific state user is looking for
     const query = ufoRef
       .orderByChild("state")
       .equalTo(`${userStateSelection}`)
-      .limitToFirst(3);
+      .limitToFirst(50);
 
     query.once("value").then((snapshot) => {
       //storing ufoSightings in state
-      setUfoSightings([snapshot.val()]);
+      snapshot.forEach((snap) => {
+        allUfo.push(snap.val());
+      });
+      setUfoSightings(allUfo);
     });
   }, [userStateSelection]);
 
-  console.log(ufoSightings);
+  //UFO sightings
+  // const results = ufoSightings.map((item) => <h1>{item.shape}</h1>);
 
   return (
-    <StateSelection
-      setUserStateSelection={setUserStateSelection}
-      userStateSelection={userStateSelection}
-    />
+    <>
+      <StateSelection
+        setUserStateSelection={setUserStateSelection}
+        userStateSelection={userStateSelection}
+      />
+    </>
   );
 };
 
